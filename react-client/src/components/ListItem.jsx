b8d252188a6b61486a4c1153172497f52ba8875b
Jay Kindell <jaykindell@gmail.com>
2018-02-06 06:07:09 -0600

    fixes link

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ListItem = (props) => (
   <div>
     <ul>
-      <li><a href= {props.item.lyricsUrl}> { props.item.songName }</a></li>
+      <li><a href= {props.item.lyricsUrl} target="_blank"> { props.item.songName }</a></li>
       <li>{ props.item.artist }</li>
     </ul>  
   </div>  
